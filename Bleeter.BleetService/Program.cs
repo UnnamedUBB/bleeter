93eaff86bf24867681548fe942b773922578f125
Gabriel Siwiec <gsiwiec3131@gmail.com>
2024-06-03 18:06:41 +0200

    fix: db context registration

--- Bleeter.BleetService/Program.cs
+++ Bleeter.BleetService/Program.cs
@@ -1,9 +1,15 @@
+using Bleeter.BleetService.DAL;
+using Bleeter.Shared.DAL.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddPersistence<BleetContext>(
+    builder.Configuration.GetConnectionString("Default") ?? throw new InvalidOperationException()
+    , "Bleeter.BleetService.DAL");
 
 var app = builder.Build();
 
